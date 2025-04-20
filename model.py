590aa34cd4ca7be34ee8d13c28f99dfcafbecf33
Michael Hylton <michaelandrewhylton@gmail.com>
2025-04-20 17:34:49 -0400

    Add files via upload

--- model.py
+++ model.py
@@ -1,19 +1,28 @@
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import UserMixin
-from flask_bcrypt import generate_password_hash, check_password_hash
+from flask_bcrypt import Bcrypt
 
 db = SQLAlchemy()
+bcrypt = Bcrypt()
 
 class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(20), unique=True, nullable=False)
     password = db.Column(db.String(100), nullable=False)
 
+    # Relationships
+    sent_messages = db.relationship('Message', foreign_keys='Message.sender_id', backref='sender', lazy='dynamic')
+    received_messages = db.relationship('Message', foreign_keys='Message.receiver_id', backref='receiver', lazy='dynamic')
+
     def set_password(self, password):
-        self.password = generate_password_hash(password).decode('utf-8')
+        self.password = bcrypt.generate_password_hash(password).decode('utf-8')
 
     def check_password(self, password):
-        return check_password_hash(self.password, password)
+        return bcrypt.check_password_hash(self.password, password)
+
+    def __repr__(self):
+        return f'<User {self.username}>'
+
 
 class Message(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -21,3 +30,6 @@ class Message(db.Model):
     receiver_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
     content = db.Column(db.Text, nullable=False)
     timestamp = db.Column(db.DateTime, default=db.func.current_timestamp())
+
+    def __repr__(self):
+        return f'<Message from {self.sender_id} to {self.receiver_id}>'
