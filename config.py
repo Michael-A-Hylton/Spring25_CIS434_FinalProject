afc6eabed4680b647acc51720bff643f2007c74a
Michael Hylton <michaelandrewhylton@gmail.com>
2025-05-30 09:33:48 -0400

    Add files via upload

--- config.py
+++ config.py
@@ -4,3 +4,9 @@ class Config:
     SECRET_KEY = os.urandom(24)  # Secure session key
     SQLALCHEMY_DATABASE_URI = 'sqlite:///database.db'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+    MAIL_SERVER = 'smtp.gmail.com'  # or your SMTP provider
+    MAIL_PORT = 587
+    MAIL_USE_TLS = True
+    MAIL_USERNAME = 'sms.2fa.verify@gmail.com'
+    MAIL_PASSWORD = 'ohze aydx fpwe gfpv'
+    MAIL_DEFAULT_SENDER = 'sms.2fa.verify@gmail.com'
